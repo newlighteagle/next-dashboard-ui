bcddf7d568175a031d474d1cee82a64e067da266
Sofyan Salim <sofyan.agus18@gmail.com>
2024-11-17 20:26:11 +0700

    add colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "Smallholder Hub",
+  description: "Smallholder Hub",
 };
 
 export default function RootLayout({
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // Primary Colors
+        wriPrimaryGold: "#F0AB00",
+        wriPrimaryBlack: "#000000",
+        wriPrimaryGray: "#9B9B9B",
+        // Secondary Colors
+        wriSecondaryBlue: "#3855A3",
+        wriSecondaryCyan: "#0096A4",
+        wriSecondaryGreen: "#32864B",
+        wriSecondaryOrange: "#EB8026",
+        wriSecondaryPink: "#EE3161",
+        wriSecondaryMagenta: "#9B216C",
+        wriSecondaryRed: "#8A1A25",
+        // Tertiary Colors
+        wriTertiaryGreen: "#81C67B",
+        wriTertiaryBlue: "#7EBCC6",
+        wriTertiaryCream: "#FBE7C5",
+        wriTertiaryGray: "#E7E6E6",
+      },
     },
   },
   plugins: [],
 };
 export default config;
+
+// WRI Colors
+// Primary
+// #F0AB00
+// Secondary
